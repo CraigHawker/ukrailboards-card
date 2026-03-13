964ac0b7baa390ba7c6aab8b6c6eb9c10ed9a7dc
Craig Hawker <craig.hawker@m-files.com>
2026-03-13 15:53:44 +0000

    Animation timing better.

--- scripts/scrolling.js
+++ scripts/scrolling.js
@@ -1,10 +1,19 @@
 (function () {
+	var BASE_CALLING_AT_DISTANCE_PX = 772 - 426;
+	var BASE_CALLING_AT_DURATION_S = 10;
+	var CALLING_AT_FIXED_TIME_S = 0.5 + 2 + 1.5 + 0.5;
+	var BASE_CALLING_AT_TRAVEL_TIME_S = BASE_CALLING_AT_DURATION_S - CALLING_AT_FIXED_TIME_S;
+
 	function measureElement(el) {
 		var availableWidth = el.getBoundingClientRect().width;
 		var actualWidth = el.scrollWidth;
+		var overflowDistance = Math.max(0, actualWidth - availableWidth);
+		var callingAtTravelDurationSeconds = (overflowDistance / BASE_CALLING_AT_DISTANCE_PX) * BASE_CALLING_AT_TRAVEL_TIME_S;
+		var callingAtDurationSeconds = CALLING_AT_FIXED_TIME_S + callingAtTravelDurationSeconds;
 
 		el.style.setProperty('--available-width', parseInt(availableWidth) + 'px');
 		el.style.setProperty('--actual-width', parseInt(actualWidth) + 'px');
+		el.style.setProperty('--calling-at-scroll-duration', callingAtDurationSeconds + 's');
 
 		if (actualWidth - 1 >= availableWidth) {
 			el.classList.add('scroll');
