f8ba27b1d0091ead4e525d4f10b2babcc4aecb32
Craig Hawker <craig.hawker@m-files.com>
2026-03-19 13:12:46 +0200

    Handlebars template now uses data.

--- scripts/data.js
+++ scripts/data.js
@@ -1,3 +1,5 @@
+registerHandlebarsHelpers();
+
 // Compile our templates.
 var layoutTemplate = Handlebars.compile(document.getElementById("template-layout").innerHTML);
 var themeTemplate = Handlebars.compile(document.getElementById("template-theme").innerHTML);
@@ -26,7 +28,7 @@ var themes = [{
 }];
 
 // Get the actual train data.
-var trainData = getTrainData();
+var boardData = getTrainData();
 
 // Iterate over every combination of theme and layout and output a board for each.
 var finalOutput = "";
@@ -41,7 +43,7 @@ for (var i = 0; i < layouts.length; i++) {
         var data = {
             layout: layouts[i],
             theme: themes[j],
-            trains: trainData
+            board: boardData
         };
         finalOutput += boardTemplate(data);
     }
@@ -49,6 +51,99 @@ for (var i = 0; i < layouts.length; i++) {
 
 // Output the final HTML.
 document.getElementById("boards").innerHTML = finalOutput;
+
+function registerHandlebarsHelpers(){
+    Handlebars.registerHelper("statusText", function(service){
+        return (service && (service.etd || service.std)) || "";
+    });
+
+    Handlebars.registerHelper("departureClass", function(service){
+        return (((service && service.etd) || "") === "On time") ? "on-time" : "";
+    });
+
+    Handlebars.registerHelper("isEstimated", function(service){
+        var status = (service && service.etd) || "";
+        var isCancelled = !!(service && service.isCancelled) || status === "Cancelled";
+        return !isCancelled && status !== "On time";
+    });
+
+    Handlebars.registerHelper("isCancelled", function(service){
+        var status = (service && service.etd) || "";
+        return !!(service && service.isCancelled) || status === "Cancelled";
+    });
+
+    Handlebars.registerHelper("firstPlatform", function(trainServices){
+        if(!trainServices || trainServices.length === 0) return "";
+        return trainServices[0].platform || "";
+    });
+
+    Handlebars.registerHelper("destinationName", function(service){
+        var currentDestinations = service && service.currentDestinations;
+        if(currentDestinations && currentDestinations.length > 0){
+            return currentDestinations[0].locationName || "";
+        }
+
+        var destinations = service && service.destination;
+        if(destinations && destinations.length > 0){
+            return destinations[0].locationName || "";
+        }
+
+        return "";
+    });
+
+    Handlebars.registerHelper("callingPoints", function(service){
+        if(!service || !service.subsequentCallingPoints || service.subsequentCallingPoints.length === 0){
+            return [];
+        }
+
+        return service.subsequentCallingPoints[0].callingPoint || [];
+    });
+
+    Handlebars.registerHelper("displayTime", function(callingPoint){
+        var estimated = (callingPoint && callingPoint.et) || "";
+        if(isClockTime(estimated)) return estimated;
+        return (callingPoint && callingPoint.st) || "";
+    });
+
+    Handlebars.registerHelper("timeDateTime", function(time, generatedAt){
+        if(!isClockTime(time)) return "";
+        var date = generatedAt ? generatedAt.slice(0, 10) : "";
+        if(!date) return "";
+        return date + "T" + time + ":00";
+    });
+
+    Handlebars.registerHelper("serviceDateTime", function(service, generatedAt){
+        return buildDateTime((service && service.std) || "", generatedAt);
+    });
+
+    Handlebars.registerHelper("callingPointDateTime", function(callingPoint, generatedAt){
+        var time = getMachineTime((callingPoint && callingPoint.et) || "", (callingPoint && callingPoint.st) || "");
+        return buildDateTime(time, generatedAt);
+    });
+
+    Handlebars.registerHelper("isLast", function(index, collection){
+        return !!collection && index === collection.length - 1;
+    });
+}
+
+function isClockTime(value){
+    return /^\d{2}:\d{2}$/.test(value || "");
+}
+
+function getMachineTime(estimatedTime, scheduledTime){
+    if(isClockTime(estimatedTime)) return estimatedTime;
+    if(isClockTime(scheduledTime)) return scheduledTime;
+    return "";
+}
+
+function buildDateTime(time, generatedAt){
+    if(!isClockTime(time)) return "";
+
+    var date = generatedAt ? generatedAt.slice(0, 10) : "";
+    if(!date) return "";
+
+    return date + "T" + time + ":00";
+}
 
 // Let's create a function for the train data so it's out the way at the bottom of the page.
 function getTrainData(){
